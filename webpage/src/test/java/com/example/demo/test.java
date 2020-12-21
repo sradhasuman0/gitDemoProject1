9c2c5195140bfec53696788a652b77aec108ae5d
SR20167957 <SR20167957@10.218.40.85>
2020-12-21 11:36:34 +0530

    junit added

---
+++ webpage/src/test/java/com/example/demo/test.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class test {
+	@Test
+	void testcase()
+	{
+		Assertions.assertEquals("hello","hello");
+	}
+	
+
+}
