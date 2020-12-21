13c6ad643621cdb9cfb5c40a2549ed5509012375
SR20167957 <SR20167957@10.218.40.85>
2020-12-21 12:55:48 +0530

    test.java deleted and added test case in webapp

--- webpage/src/test/java/com/example/demo/WebpageApplicationTests.java
+++ webpage/src/test/java/com/example/demo/WebpageApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,8 @@ class WebpageApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals("hello","hello");
 	}
+	
 
 }
